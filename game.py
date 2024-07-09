ef9e3f917a93362bc8f69da429b292fef1d251f8
1dhruvsingh <dhruvsinghsir@gmail.com>
2024-07-10 02:26:05 +0530

    Made some progress
    connected to main and board (creating pieces)

--- game.py
+++ game.py
@@ -1,15 +1,33 @@
 import pygame
 from const import *
+from board import Board
 
 class  Game:
     
     def __init__(self):
-        pass
+        self.baord = Board()
     
     #show methods
     
     def show_bg(self,surface):
         for row in range(ROWS):
             for col in range(COLS):
+                if (row + col ) % 2== 0:
+                    colour = (234,235,200) #light green
+                else:
+                    colour= (119,154,88) #dark green
+                    
+                rect = (col * SQSIZE,row * SQSIZE , SQSIZE ,SQSIZE)
+                pygame.draw.rect(surface,colour,rect)
                 
+    def show_pieces(self,surface):
+        for row in range(ROWS):
+            for col in range(COLS):
+                #piece?
+                if self.board.squares[row][col].has_piece():
+                    piece = self.board.squares[row][col].has_piece
+                    img= pygame.image.load(piece.texture)
+                    img_center = col* SQSIZE + SQSIZE //2 , row* SQSIZE +SQSIZE//2
+                    piece.texture_rect=img.et_rect(center= img_center)
+                    surface.blit(img ,piece.texture_rect)
             
---
+++ main.py
@@ -0,0 +1,36 @@
+
+import pygame
+import sys
+
+from const import *
+from game import Game
+from square import Square
+from move import Move
+
+class Main:
+    
+    def __init(self):
+        pygame.init()
+        self.screen=pygame.display.set_mode((WIDTH,HEIGHT))
+        pygame.display.set_caption('Chess')
+        self.game =Game
+        
+    def mainloop(self):
+        
+        screen =self.screen
+        game =self.game
+        
+        while True:
+            game.show_bg(screen)
+            game.show_pieces(screen)
+    
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                
+            pygame.display.update()
+
+main=Main()
+main.mainloop()            
+        
