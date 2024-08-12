5d92653a065e3a17b0b1afcd517febf0d339b18c
Dhruv Singh <dhruvsinghsir@gmail.com>
2024-08-12 23:24:23 +0530

    edits of mouse and click in the #main
    click and drag feature added

--- main.py
+++ main.py
@@ -31,19 +31,59 @@ class Main:
          game.show_hover(screen)
          
     
-         if dragger.dragging:
-           dragger.update_blit(screen)
+             if dragger.dragging:
+                dragger.update_blit(screen)
         
-         for event in pygame.event.get():
+             for event in pygame.event.get():
         
-           #click
-           if event.type == pygame.MOUSEBUTTONDOWN:
-              dragger.update_mouse(event.pos)
+                #click
+                  if event.type == pygame.MOUSEBUTTONDOWN:
+                    dragger.update_mouse(event.pos)
             
-              clicked_row= dragger.mouseY//SQSIZE
-              clicked_col= dragger.mousex//SQSIZE
+                    clicked_row= dragger.mouseY//SQSIZE
+                    clicked_col= dragger.mousex//SQSIZE
             
-            #if clicked 
+                   #if clicked square has a piece ?
+                   if board.squares[clicked_row][clicked_col].has_piece():
+                      piece=board.squares[clicked_row][clicked_col].piece
+                      #valid piece(color) ?
+                      if piece.color==game.next_player:
+                          board.calc_moves(piece,clicked_row,clicked_col,bool=True)
+                          dragger.save_initial(event.pos)
+                          dragger.drag_piece(piece)
+                          #show methods
+                          game.show_bg(screen)
+                          game.show_last_move(screen)
+                          game.show_moves(screen)
+                          game.show_pieces(screen)
+                 
+                   #mouse motion
+                   elif event.type==gygame.MOUSEMOTION:
+                       motion_row=event.pos[1]//SQSIZE
+                       motion_col=event.pos[0]//SQSIZE
+              
+                       game.set_hover(motion_row,motion_col)
+              
+                       if dragger.dragging:
+                           dragger.update_mouse(event.pos)
+                           #show methods
+                           game.show_bg(screen)
+                           game.show_last_move(screen)
+                           game.show_moves(screen)
+                           game.show_pieces(screen)
+                           game.show_hover(screen)
+                           dragger.update_blit(screen)
+                           
+                   #click release
+                   elif event.type==pygame.MOUSEBUTTONUP:
+                     
+                       #if piece is being dragged
+                       if dragger.dragging:
+                         dragger.update_mouse(event.pos)
+                
+                
+                
+            
 
         
    
